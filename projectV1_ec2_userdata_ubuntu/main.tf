f44adc3de98bc95b31dba5a3eb9c41de106b5dcd
SunsFeed <sun12wook11@gmail.com>
2024-09-11 16:30:13 +0900

    projectV1_ec2_userdata_ubuntu

---
+++ projectV1_ec2_userdata_ubuntu/main.tf
@@ -0,0 +1,11 @@
+data "template_file" "userdata_ubuntu" {
+  template = <<-EOF
+    sudo timedatectl set-timezone "Asia/Seoul"
+    sudo hwclock
+    sudo sed -i 's/#Port 22/Port 12137/' /etc/ssh/sshd_config
+    sudo systemctl restart sshd
+    sudo apt -y install language-pack-ko
+    sudo localectl set-locale LANG=ko_KR.UTF-8
+    sudo hostnamectl set-hostname projectOne
+  EOF
+}
