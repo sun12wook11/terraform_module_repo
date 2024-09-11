81473563eb6afd558be6e828f1815f72e447d593
SunsFeed <sun12wook11@gmail.com>
2024-09-11 14:20:36 +0900

    ec2_instance_ubuntu

---
+++ ec2_instance_ubuntu/variables.tf
@@ -0,0 +1,25 @@
+#여기있는 애들 쓰는게 아니고 가져온 깃허브에서 참조 중이기에 일단 주석
+variable "ami" {
+  description = "ami of instance"
+  type = string
+}
+variable "instance_type" {
+  description = "instance_type of instance"
+  type = string
+}
+variable "key_name" {
+  description = "key_name of instance"
+  type = string
+}
+variable "instance_name" {
+  description = "instance_name of instance"
+  type = string
+}
+variable "security_group_id" {
+  description = "security_group_id of instance"
+  type = string
+}
+variable "user_data" {
+  description = "user script to execute"
+  type = string
+}
