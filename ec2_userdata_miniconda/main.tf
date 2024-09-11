3a9b2a399acd5e9b3a14b4ca34dfe570da42add6
SunsFeed <sun12wook11@gmail.com>
2024-09-11 14:37:45 +0900

    Update main.tf

--- ec2_userdata_miniconda/main.tf
+++ ec2_userdata_miniconda/main.tf
@@ -1,6 +1,6 @@
 # miniconda를 설치하는 user-data
 
-data "template_file" "userdata_miniconda" {
+data "template_file" "miniconda_userdata" {
   template = <<-EOF
     wget https://repo.anaconda.com/miniconda/Miniconda3-py310_24.7.1-0-Linux-x86_64.sh -O /tmp/Miniconda3.sh
     bash /tmp/Miniconda3.sh -b -p /home/ubuntu/miniconda3
