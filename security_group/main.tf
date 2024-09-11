d8f3a4c3a7d198c1055ad15683016f558fcb9466
SunsFeed <sun12wook11@gmail.com>
2024-09-11 15:52:07 +0900

    Update main.tf

--- security_group/main.tf
+++ security_group/main.tf
@@ -3,16 +3,30 @@ resource "aws_security_group" "this" {
   name          = var.sg_name
   description   = var.description
   ingress {
-    description = "Allow mariadb from anywhere"
-    from_port = var.ingress_port
-    to_port = var.ingress_port
+    description = "Allow fastapi from anywhere"
+    from_port = var.ingress_port_fastapi
+    to_port = var.ingress_port_fastapi
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    description = "Allow SSH from anywhere"
-    from_port = 22
-    to_port = 22
+    description = "Allow nginx from anywhere"
+    from_port = var.ingress_port_nginx
+    to_port = var.ingress_port_nginx
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow mariadb from anywhere"
+    from_port = var.ingress_port_mariadb
+    to_port = var.ingress_port_mariadb
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow ssh from anywhere"
+    from_port = var.ingress_port_ssh
+    to_port = var.ingress_port_ssh
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
