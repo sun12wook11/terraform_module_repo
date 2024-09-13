7e20c791212df817cd60ed6ceca53582b789d4da
SunsFeed <sun12wook11@gmail.com>
2024-09-13 17:55:24 +0900

    Update variables.tf

--- aws_vpc_private_subnet_fastapi/variables.tf
+++ aws_vpc_private_subnet_fastapi/variables.tf
@@ -6,10 +6,6 @@ variable "public_subnet_nginx_igw_id" {
   description = "VPC ID to associate with the subnet."
   type        = string
 }
-variable "public_subnet_nginx_id" {
-  description = "CIDR block for the private subnet."
-  type        = string
-}
 variable "private_subnet_fastapi_cidr_block" {
   description = "CIDR block for the private subnet."
   type        = string
