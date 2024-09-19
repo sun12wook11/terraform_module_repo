102a60342e30c00fae4076821064756b3f8d338b
SunsFeed <sun12wook11@gmail.com>
2024-09-19 15:17:42 +0900

    changes

--- private_subnet/outputs.tf
+++ private_subnet/outputs.tf
@@ -1,7 +1,10 @@
-
 output "private_subnet_sg_id" {
-  value = aws_security_group.private_sg.id
+  value = {
+    for key, sg in aws_security_group.private_sg :
+    key => sg.id
+  }
 }
+
 
 output "subnets" {
   value = aws_subnet.private_subnet
